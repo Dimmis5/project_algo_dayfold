83d4f8e19cc35a054f9591864e67fc046eb00832
Dimmis5 <zhangdimeo@gmail.com>
2026-04-28 19:42:55 +0200

    [rest] first version

--- app/main.py
+++ app/main.py
@@ -1,21 +1,6 @@
 import time
-from neo4j.exceptions import ServiceUnavailable
 from models import DayfoldGraph
 from visualizer import Neo4jVisualizer
-
-def wait_for_neo4j(max_retries=30, delay=10):
-    print("Wait 20s for Neo4j...")
-    time.sleep(20)
-    for attempt in range(max_retries):
-        try:
-            viz = Neo4jVisualizer()
-            viz.driver.verify_connectivity()
-            print("Neo4j ready ")
-            return viz
-        except Exception as e:
-            print(f"Try {attempt + 1}/{max_retries} - Neo4j not ready yet: {e}")
-            time.sleep(delay)
-    raise RuntimeError("Bug")
 
 def run_app():
     net = DayfoldGraph()
@@ -29,10 +14,16 @@ def run_app():
     net.add_friendship(3, 4)
     net.add_friendship(4, 1)
 
-    viz = wait_for_neo4j()
-    viz.sync_graph(net)
-    viz.close()
-    print("Graphe Succes")
+    print("Tentative de connexion à Neo4j...")
+    time.sleep(10) 
+    
+    try:
+        viz = Neo4jVisualizer()
+        viz.sync_graph(net)
+        viz.close()
+        print("Graphe synchronisé avec succès !")
+    except Exception as e:
+        print(f"Erreur : {e}")
 
 if __name__ == "__main__":
     run_app()
